dd4e46ab1996fcf0cb532ab6680baeac7cd259e8
kgh <gihyeon6394@gmail.com>
2023-09-25 09:25:26 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Demo Clean Architecture
+
+- clean architecture를 spring boot로 구현
+
+### 개발환경
+
+- Java 17
+- Spring Boot 3.1.4
+- Gradle
+- Junit 5
+- JPA Hibernate
+
+### reference
+
+- [Baeldung Clean Architecture with Spring Boot](https://www.baeldung.com/spring-boot-clean-architecture)
---
+++ src/main/java/com/example/democleanarchi/DemocleanarchiApplication.java
@@ -0,0 +1,14 @@
+package com.example.democleanarchi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemocleanarchiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemocleanarchiApplication.class, args);
+        System.out.println("Hello Demo Clean Archi!");
+    }
+
+}
